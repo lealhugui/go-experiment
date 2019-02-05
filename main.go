db0d4b041aceb99694bde9e79f428dedbded1aa1
Guilherme Leal <lealhugui@gmail.com>
2019-02-05 15:08:22 -0200

    updating go package manager

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	var chosenUndead WarriorOfSunlight = &Undead{"Solaire"}
+	fmt.Printf("%s\n", chosenUndead.PraiseTheSun())
+}
+
+// WarriorOfSunlight is an Interface for the covenant of the sun
+type WarriorOfSunlight interface {
+	PraiseTheSun() string
+}
+
+// Undead is the common race
+type Undead struct {
+	name string
+}
+
+// PraiseTheSun is an action that the Undead do as they are warrriors of sunlight
+func (c *Undead) PraiseTheSun() string {
+	return fmt.Sprint("\\[T]/ PRAISE THE SUN \\[T]/")
+}
